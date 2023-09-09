ba2d918e8fedc97b69519bb307ec4eca16a08647
David Llanos <david.llanos@pucp.edu.pe>
2023-09-08 21:58:23 -0500

    segundo avance

--- src/main/java/com/example/lab4_20201638_gtics/controller/EmployeeController.java
+++ src/main/java/com/example/lab4_20201638_gtics/controller/EmployeeController.java
@@ -5,7 +5,9 @@ import com.example.lab4_20201638_gtics.repository.EmployeeRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -25,5 +27,16 @@ public class EmployeeController {
         model.addAttribute("employeeList", lista);
 
         return "lista";
+    }
+
+    @PostMapping("/buscarPorNombre")
+    public String buscarPorNombre(@RequestParam("searchField") String searchField, Model model) {
+
+        //List<Shipper> lista =  shipperRepository.findByCompanyName(searchField);
+        List<Employee> lista = employeeRepo.(searchField);
+        model.addAttribute("employee", lista);
+        model.addAttribute("textoBuscado", searchField);
+
+        return "shipper/list";
     }
 }
--- src/main/java/com/example/lab4_20201638_gtics/repository/employeeRepo.java
+++ src/main/java/com/example/lab4_20201638_gtics/repository/employeeRepo.java
@@ -3,10 +3,15 @@ package com.example.lab4_20201638_gtics.repository;
 import com.example.lab4_20201638_gtics.Entity.Countrie;
 import com.example.lab4_20201638_gtics.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+   // @Query(nativeQuery = true, value = "select * from employees where  department_id  like '%?%')
+    // List<Employee> buscarPorIdDepartment (Integer numero);
 
 }
 
