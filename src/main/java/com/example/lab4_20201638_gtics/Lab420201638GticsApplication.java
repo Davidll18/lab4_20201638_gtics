776b144d5fc068c3b7c66b9911308b15dfb9396b
David Llanos <david.llanos@pucp.edu.pe>
2023-09-08 20:33:14 -0500

    first commit

---
+++ src/main/java/com/example/lab4_20201638_gtics/Lab420201638GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20201638_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420201638GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420201638GticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4_20201638_gtics/Lab420201638GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20201638_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420201638GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
